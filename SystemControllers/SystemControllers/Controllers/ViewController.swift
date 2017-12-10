8e24f65c86aacda24c1c54e7f4fb83ce827758ce
Adrien Meyer <reyemneirda@gmail.com>
2017-12-10 14:57:17 +0200

    Safari function added, going to youtube

--- SystemControllers/SystemControllers/Controllers/ViewController.swift
+++ SystemControllers/SystemControllers/Controllers/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import MessageUI
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var photoButton: UIButton!
+    
+    @IBOutlet weak var emailButton: UIButton!
+    @IBOutlet weak var safariButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -25,17 +32,43 @@ class ViewController: UIViewController {
         present(activityController, animated: true, completion: nil)
     }
 
-    @IBAction func takePhoto(_ sender: Any) {
+    @IBAction func takePhoto(_ sender: Any)
+    {
         
     }
     
-    @IBAction func safariButton(_ sender: Any) {
+    @IBAction func safariButton(_ sender: Any)
+    {
         
-        
+            if let urls = URL(string: "https://www.youtube.com/watch?v=tZp8sY06Qoc")
+            {
+                let safariVC: SFSafariViewController = SFSafariViewController(url: urls)
+                self.present(safariVC, animated: true, completion: nil)
+                
+            }
+            
     }
     
-    @IBAction func emailButton(_ sender: Any) {
+    @IBAction func emailButton(_ sender: UIButton)
+    {
         
+        if !MFMailComposeViewController.canSendMail()
+        {
+            print("Can not send mail")
+            
+            return
+        }
+        
+        let mailVC: MFMailComposeViewController = MFMailComposeViewController()
+        mailVC.mailComposeDelegate = self
+        
+        if self.safariButton.isEnabled
+        {
+            let imageData: Data = UIImagePNGRepresentation(self.imageView.image!) as! Data
+            mailVC.addAttachmentData(imageData, mimeType: "jpg", fileName: "mySharedImage")
+        }
+        
+        self.present(mailVC, animated: true, completion: nil)
     }
     
     
